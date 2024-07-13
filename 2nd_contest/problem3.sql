715086b137345a1d95d87130b86a679a9532d23d
kdokan <kotarodokan1219@gmail.com>
2024-07-13 17:02:58 +0900

    修正

--- 2nd_contest/problem3.sql
+++ 2nd_contest/problem3.sql
@@ -1,23 +1,26 @@
 SELECT
     d.pf_code as CODE
-    , pf_name as NAME
+    , pf_name as "NAME"
     , ROUND(
           (SUM(CASE WHEN CATEGORY_CODE = '120' THEN AMT ELSE 0 END) * 1.0 
             / SUM(CASE WHEN CATEGORY_CODE = '110' THEN AMT ELSE 0 END) * 1.0) * 100, 1
-           ) AS PERCENTAGE
+           ) AS "PERCENTAGE"
 FROM
     drink_habits as d
 
-LEFT JOIN
+INNER JOIN
         prefecture as p
     ON
         d.pf_code = p.pf_code
+        
+WHERE
+    d.GENDER_CODE IN ('2', '3') 
 
 GROUP BY
     d.pf_code
 
 ORDER BY
-    percentage DESC
-    , code ASC
+    PERCENTAGE DESC
+    , CODE DESC
 
 ;
