9986b68f7750b4c380982617d369868fc35abcc0
kdokan <kotarodokan1219@gmail.com>
2024-07-13 16:48:19 +0900

    初回提出

---
+++ 2nd_contest/problem3.sql
@@ -0,0 +1,22 @@
+SELECT
+    d.pf_code as CODE
+    , pf_name as NAME
+    , ROUND(
+          (SUM(CASE WHEN CATEGORY_CODE = '120' THEN AMT ELSE 0 END) * 1.0 / SUM(AMT)) * 100, 1
+           ) AS PERCENTAGE
+FROM
+    drink_habits as d
+
+LEFT JOIN
+        prefecture as p
+    ON
+        d.pf_code = p.pf_code
+
+GROUP BY
+    d.pf_code
+
+ORDER BY
+    percentage DESC
+    , code ASC
+
+;
